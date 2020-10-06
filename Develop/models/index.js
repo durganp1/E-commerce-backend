e624ce561d89a274bef9f87f904107ee9f826abc
durganp1 <durganp1@gmail.com>
2020-10-06 11:00:54 -0500

    added final routes

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -16,13 +16,13 @@ Category.hasMany(Product, {
 Product.belongsToMany(Tag, {
   through: ProductTag,
   as: 'product_tags',
-  foreignKey: 'tag_id'
+  foreignKey: 'product_id'
 });
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: ProductTag,
   as: 'product_tags',
-  foreignKey: 'tag_id'
+  foreignKey: 'product_id'
 });
 
 
